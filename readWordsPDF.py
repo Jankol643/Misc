f972dc56c4cd6e20705e6fd05d5de93a4d4e5a9d
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-07-09 07:14:00 +0200

    syntax corrections

--- readWordsPDF.py
+++ readWordsPDF.py
@@ -4,7 +4,7 @@ import pdfplumber
 
 def checkTools():
     requiredTools = ["pdfplumber"]
-    for tool in requiredTools
+    for tool in requiredTools:
         if (is_tool(tool) == False):
             print("Required tool " + tool + "does not exist. Aborting")
             sys.exit(-1)
@@ -13,24 +13,24 @@ def is_tool(name):
     # Check whether `name` is on PATH and marked as executable.
     from shutil import which
     
-    if which(name) is not None
+    if which(name) is not None:
         return True
-    else
+    else:
         return False
 
 def readCode():
     result = list()
     def askForFilepath(filetypeName, extension):
-    root = Tk() # pointing root to Tk() to use it as Tk() in program.
-    root.withdraw() # Hides small tkinter window.
-    root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
-    extension = '*.' + extension
-    file = askopenfile(mode ='r', filetypes =[(filetypeName, extension)])
-    if file is not None
-        return file
-    else
-        print("File not found. Aborting")
-        sys.exit(-1)
+        root = Tk() # pointing root to Tk() to use it as Tk() in program.
+        root.withdraw() # Hides small tkinter window.
+        root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
+        extension = '*.' + extension
+        file = askopenfile(mode ='r', filetypes =[(filetypeName, extension)])
+        if file is not None:
+            return file
+        else:
+            print("File not found. Aborting")
+            sys.exit(-1)
         
     codeFile = askForFilepath("Text files", "txt")
     pdfFile = askForFilepath("PDF files", "pdf")
@@ -41,6 +41,12 @@ def readCode():
                 processLine(line)
                 
     def processLine(line):
+        def getWord(pdf, page, line, word):
+            page = pdf.pages[page]
+            lineString = page.extractText()
+            lineArray = lineString.split(" ")
+            word = lineArray[word-1]
+        
         splitLine = line.split(".")
         page = splitLine[0]
         line = splitLine[1]
@@ -49,10 +55,3 @@ def readCode():
             word = getWord(pdf, page, line, word)
             result.append(word)
         return result
-            
-        def getWord(pdf, page, line, word)
-            page = pdf.pages[page]
-            lineString = page.extractText()
-            lineArray = lineString.split(" ")
-            word = lineArray[word-1]
-    
