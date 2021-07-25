558da97089e850b1c0ebd1a7b929f83343a5b415
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-07-25 13:01:50 +0200

    fixed imports

--- cleaner.py
+++ cleaner.py
@@ -9,24 +9,22 @@ Dependencies: bc for calculation, time for measuring execution time, awk, wc
 """
 
 # Import local scripts
-from cleanerUtil import * as util
+import masterUtil
+import cleanerUtil
 
-util.print_header()
+cleanerUtil.print_header()
 
 copyright='(c) 2021 Jankol643'
 print(copyright)
 
 # TODO: Dependency check
-# TODO: flags (CLI arguments)
-# TODO: file names (check CopyDefinite)
 # TODO: display tqdm progress bar for certain functions
 
-filename = sys.argv[1]
-
-util.checkFile(filename)
-util.print_info_wrapper(filename)
-util.ordering(filename)
-util.wordcount()
-util.filesize(filename, 0)
-util.filesize(filename, 1)
-util.readfilepath()
+filename = masterUtil.askForFileOrDirectory('file', 'open', 'txt')
+masterUtil.checkFile(filename)
+cleanerUtil.print_info_wrapper(filename)
+cleanerUtil.ordering(filename)
+cleanerUtil.wordcount()
+cleanerUtil.filesize(filename, 0)
+cleanerUtil.filesize(filename, 1)
+cleanerUtil.readfilepath()
