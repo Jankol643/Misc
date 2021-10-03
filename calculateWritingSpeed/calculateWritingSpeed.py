10432d5bd4c72341da356ee84a975997a7c3d488
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2021-10-03 19:07:45 +0200

    added input check and calculation

--- calculateWritingSpeed/calculateWritingSpeed.py
+++ calculateWritingSpeed/calculateWritingSpeed.py
@@ -11,7 +11,29 @@ import internet
 from datetime import datetime as DateTime, timedelta as TimeDelta # for setting timeout
 
 def calculate_speed(seconds):
-    
+    """
+    Calculates writing speed for a given text in words per minute
+    :int seconds: number of seconds
+    """
+
+    def calculate_WPM(text, seconds):
+        """
+        Calculates words per minute
+        :string text: text to count WPM for
+        :int seconds: number of seconds
+        """
+        word_count = masterUtil.count_words(text)
+        if seconds > 60:
+            WPM = word_count/(seconds/60)
+        else:
+            var = 60/seconds
+            WPM = word_count * var
+        return WPM
+
+    def check_input(seconds):
+        if seconds <= 0:
+            raise ValueError("Entered time must be greater than 0 seconds.")
+
     def decide_text():
         """
         Asks the user if the text should come from the internet or a file
@@ -50,18 +72,19 @@ def calculate_speed(seconds):
         print("Time is up.")
         return written_text
 
+    check_input(seconds)
     decision = decide_text()
     if (decision == 'internet'):
         text = internet.getTextFromURL()
     elif (decision == 'file'):
         filepath = input("Please specify a filepath: ")
+        if filepath == "":
+            filepath = internet.get_data_path()
         text = fileUtil.read_line_file(filepath, 1)
     
     print_text(text)
     written_text = time_typing()
-    word_count = masterUtil.count_words(written_text)
-    WPM = word_count/(seconds/60)
-    return WPM
+    calculate_WPM(written_text, seconds)
 
 if __name__ == '__main__':
     seconds = int(input("Please enter how many seconds the test should take: "))
